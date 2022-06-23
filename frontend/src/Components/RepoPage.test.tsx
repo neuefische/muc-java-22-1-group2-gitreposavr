7f3a88e51a03a34a4b0ac5f9efdb6ecec0f3e7e5
Auamann <simon-reise94@web.de>
2022-06-23 17:38:48 +0200

    given Testblock initialisiert

--- frontend/src/Components/RepoPage.test.tsx
+++ frontend/src/Components/RepoPage.test.tsx
@@ -1,3 +1,14 @@
-test('if components are rendered and correct', () => {
+import RepoPage from "./RepoPage";
+import {render} from "@testing-library/react";
+import {MemoryRouter, Routes,Route} from "react-router-dom";
 
+
+test('if components are rendered and correct', () => {
+//given
+
+    render(<MemoryRouter initialEntries={["/repos/Testusername"]}>
+        <Routes>
+            <Route path={"/repos/:Testusername"} element={<RepoPage/>}/>
+        </Routes>
+    </MemoryRouter>)
 })
